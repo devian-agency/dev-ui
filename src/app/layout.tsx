f3e0e749cfb8c71ac97619eee48e4012ee818916
Gajendrasuman <gajendrasuman868@gmail.com>
2025-10-06 18:47:29 +0530

    Backup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
-  title: "dev.ui",
+  title: "Dev UI",
   description: "devian's official UI library",
 };
 
--- src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <h1>dev.ui</h1>;
-}
